e90f208e3ccebb26d14efd28669ce4bb21edcf57
Arun Selvaraj <arun18@live.in>
2025-12-23 06:23:43 +0530

    feat: add Vercel Speed Insights

--- study-planner/src/app/layout.tsx
+++ study-planner/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
@@ -46,6 +47,7 @@ export default function RootLayout({
           </NativeMobileSpacer>
           <MobileBottomNav />
           <WhatsAppButton />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
