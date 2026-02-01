20cc727daf3fe257057d8f27ddb2dd9a2f0310f9
Arun Selvaraj <arun18@live.in>
2026-02-01 20:32:50 +0530

    Seed 50 new discount coupons

--- study-planner/scripts/seed_coupons.js
+++ study-planner/scripts/seed_coupons.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 
+// Ideally this should come from process.env, but hardcoded for the script as per previous file content
 const MONGODB_URI = "mongodb+srv://admin_db_user:Dak%40639104@cluster0.jawkuep.mongodb.net/study-planner?appName=Cluster0";
 
 const coupons = [
-    "T9L3X4",
-    "W2N8V7"
+    "DG7K2P", "XJ9M4W", "BT5N8R", "QZ1V6Y", "HK3L9M",
+    "P4W7NB", "CV2M9K", "RT8X5Z", "GN6P1Q", "YJ3D7W",
+    "MK9L2V", "BR4H7N", "XF5K8P", "TW1Z6M", "QS3G9D",
+    "NP7W2X", "VK4M8R", "HY1B6Z", "LT5N9P", "JC2K7W",
+    "GD8X3M", "RZ6P1V", "KW4L9N", "BM2H7Q", "TV5Z8K",
+    "PL9N3W", "HX7K2M", "CR5V8Z", "BK1G6P", "MW4Q9D",
+    "ZF7L2N", "VD5H8K", "RT3P9W", "XJ1M6V", "GY4B7Q",
+    "KS8N2M", "PW6V3Z", "HL9K1D", "BT4R7X", "NV2M5P",
+    "QW7Z3K", "MJ9N1G", "BR5L8V", "XH2D6W", "TK4P9N",
+    "PV1G7M", "KZ8X3W", "HN5M2Q", "BC7L9R", "WF4K2Z"
 ];
 
 // Define Schema locally to avoid import issues
